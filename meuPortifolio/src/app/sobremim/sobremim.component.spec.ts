ff6e72ce03ba60484ca0e2b95f32ceb22ce6846b
eduardocarlosd <eduardocarlosdias1@gmail.com>
2024-08-29 20:38:48 -0300

    subindo aplicação

---
+++ meuPortifolio/src/app/sobremim/sobremim.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SobremimComponent } from './sobremim.component';
+
+describe('SobremimComponent', () => {
+  let component: SobremimComponent;
+  let fixture: ComponentFixture<SobremimComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SobremimComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SobremimComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
