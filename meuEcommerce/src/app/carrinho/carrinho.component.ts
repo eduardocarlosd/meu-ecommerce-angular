4b8e633491ec43582262af67ec070cfbdb4797da
eduardocarlosd <eduardocarlosdias1@gmail.com>
2024-09-02 21:57:33 -0300

    mudando nome da pasta

---
+++ meuEcommerce/src/app/carrinho/carrinho.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+//import { Component, OnInit } from '@angular/core';
+// import { CarrinhoService } from '../../services/carrinho.service';
+
+@Component({
+  selector: 'app-carrinho',
+  templateUrl: './carrinho.component.html',
+  styleUrl: './carrinho.component.scss'
+})
+
+export class CarrinhoComponent {
+
+}
+
+
+
+// export class CarrinhoComponent implements OnInit {
+//   items = [];
+
+//   constructor(private carrinhoService: CarrinhoService) { }
+
+//   ngOnInit(): void {
+//     // Inicializando items com os itens do carrinho
+//     this.items = this.carrinhoService.getItems();
+//   }
+
+// }
